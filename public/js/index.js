922c71692a35ce1dddca00af4c4ecbe4ec0774e7
jerry-dbit <223jeremiah0043@dbit.in>
2025-04-20 17:48:21 +0530

    Delete public directory

--- public/js/index.js
+++
@@ -1,8 +0,0 @@
-window.onload = () => {
-    if(!sessionStorage.name){
-        location.href = '/login.html';
-    }
-    else{
-        document.getElementsById('greeting').innerText = `Hello, ${sessionStorage.name}!`;
-    }
-} 
--- public/js/name.js
+++
@@ -1,8 +0,0 @@
-window.onload = () => {
-    if(!sessionStorage.name){
-        document.getElementById('getStarted').innerText = `Hello!`;
-    }
-    else{
-        document.getElementById('getStarted').innerText = `Hello, ${sessionStorage.name}!`;
-    }
-} 
